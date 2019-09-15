49a5c9f8f1332f123044ae5f6dd2377d83e87f97
Riposte <redriposte@gmail.com>
2019-09-15 16:02:44 +0200

    Menu toggle

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,32 +4,36 @@ $headerLinks.addEventListener('mouseover', handleMenu);
 $headerLinks.addEventListener('mouseout', handleMenu);
 
 function handleMenu(e) {
-    if (e.target.tagName === "A" && e.target.parentNode.classList.length > 0) {
-        const anchor = e.target.parentNode.classList[0];
-        const menu = document.querySelector(`.menu__sub > .${anchor}`);
-        if (e.type === 'mouseover') {
-            console.log('1');
-            toVisible(menu);
-        } else {
-            menu.addEventListener('mouseover', (e) => {
-                toVisible(menu);
-            })
-            menu.addEventListener('mouseout', (e) => {
-                toInvisible(menu);
-            })
-            toInvisible(menu);
-        }
-    }
+
 }
 
-function toVisible(el) {
-    console.log('over');
-    el.style.opacity = 1;
-    el.style.visibility = "visible";
-}
+// function handleMenu(e) {
+//     if (e.target.tagName === "A" && e.target.parentNode.classList.length > 0) {
+//         const anchor = e.target.parentNode.classList[0];
+//         const menu = document.querySelector(`.menu__sub > .${anchor}`);
+//         if (e.type === 'mouseover') {
+//             console.log('1');
+//             toVisible(menu);
+//         } else {
+//             menu.addEventListener('mouseover', (e) => {
+//                 toVisible(menu);
+//             })
+//             menu.addEventListener('mouseout', (e) => {
+//                 toInvisible(menu);
+//             })
+//             // toInvisible(menu);
+//         }
+//     }
+// }
 
-function toInvisible(el) {
-    console.log('leave');
-    el.style.opacity = 0;
-    el.style.visibility = "hidden";
-}
+// function toVisible(el) {
+//     console.log('over');
+//     el.style.opacity = 1;
+//     el.style.visibility = "visible";
+// }
+
+// function toInvisible(el) {
+//     console.log('leave');
+//     el.style.opacity = 0;
+//     el.style.visibility = "hidden";
+// }
--- dist/main.cea5deef.js
+++ dist/main.cea5deef.js
@@ -122,37 +122,34 @@ var $headerLinks = document.querySelector('.header__nav');
 $headerLinks.addEventListener('mouseover', handleMenu);
 $headerLinks.addEventListener('mouseout', handleMenu);
 
-function handleMenu(e) {
-  if (e.target.tagName === "A" && e.target.parentNode.classList.length > 0) {
-    var anchor = e.target.parentNode.classList[0];
-    var menu = document.querySelector(".menu__sub > .".concat(anchor));
-
-    if (e.type === 'mouseover') {
-      console.log('1');
-      toVisible(menu);
-    } else {
-      menu.addEventListener('mouseover', function (e) {
-        toVisible(menu);
-      });
-      menu.addEventListener('mouseout', function (e) {
-        toInvisible(menu);
-      });
-      toInvisible(menu);
-    }
-  }
-}
-
-function toVisible(el) {
-  console.log('over');
-  el.style.opacity = 1;
-  el.style.visibility = "visible";
-}
-
-function toInvisible(el) {
-  console.log('leave');
-  el.style.opacity = 0;
-  el.style.visibility = "hidden";
-}
+function handleMenu(e) {} // function handleMenu(e) {
+//     if (e.target.tagName === "A" && e.target.parentNode.classList.length > 0) {
+//         const anchor = e.target.parentNode.classList[0];
+//         const menu = document.querySelector(`.menu__sub > .${anchor}`);
+//         if (e.type === 'mouseover') {
+//             console.log('1');
+//             toVisible(menu);
+//         } else {
+//             menu.addEventListener('mouseover', (e) => {
+//                 toVisible(menu);
+//             })
+//             menu.addEventListener('mouseout', (e) => {
+//                 toInvisible(menu);
+//             })
+//             // toInvisible(menu);
+//         }
+//     }
+// }
+// function toVisible(el) {
+//     console.log('over');
+//     el.style.opacity = 1;
+//     el.style.visibility = "visible";
+// }
+// function toInvisible(el) {
+//     console.log('leave');
+//     el.style.opacity = 0;
+//     el.style.visibility = "hidden";
+// }
 },{}],"assets/js/main.js":[function(require,module,exports) {
 "use strict";
 
